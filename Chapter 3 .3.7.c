2696c66ac4291158d872b5c946cfd3088cafb99f
haemos <65495522+haemos@users.noreply.github.com>
2020-05-28 09:50:53 +0800

    Update Chapter 3 3.7

--- Chapter 3 .3.7.c
+++ Chapter 3 .3.7.c
@@ -1,4 +1,3 @@
-//Write a program to solve the multiplication of two polynomials and only can use one linked list.
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -14,14 +13,12 @@ struct Polynomial_head{
 };
 typedef Polynomial* poly;
 
-poly fill_leftnext(poly leftnext) {
+poly fill_leftnext(poly leftnext,int cofficients,int power) {
 	poly fnext = (struct Polynomial*)malloc(sizeof(struct Polynomial));
 	poly fnext_next = leftnext->next;
 	poly flast_next = leftnext;
-	printf("enter the cofficients\n");
-	scanf_s("%d", &fnext->coefficient);
-	printf("enter the power\n");
-	scanf_s("%d", &fnext->power);
+	fnext->coefficient = cofficients;
+	fnext->power = power;
 
 	if (!fnext_next){
 		fnext->next = leftnext->next;
@@ -32,6 +29,10 @@ poly fill_leftnext(poly leftnext) {
 			{
 				fnext->next = fnext_next;
 				flast_next->next = fnext;
+				return leftnext;
+			}
+			else if (fnext->power == fnext_next->power) {
+				fnext_next->coefficient = fnext->coefficient + fnext_next->coefficient;
 				return leftnext;
 			}
 			if(!fnext_next->next){
@@ -45,17 +46,72 @@ poly fill_leftnext(poly leftnext) {
 	}
 	return leftnext;
 }
+poly free_next(poly leftnext, poly rightnext,poly other) {
+	poly rnext = rightnext->next;
+	poly lnext = leftnext->next;
+	poly saveleft, saveright;
+	while (rnext||lnext)
+	{
+		saveleft = lnext;
+		saveright = rnext;
+		if(lnext)lnext = lnext->next;
+		if(rnext)rnext = rnext->next;
+		if(saveright)free(saveright);
+		if (saveleft)free(saveleft);
+	}
 
+	leftnext = other;
+	return leftnext;
+}
+
+poly multipliacation(poly leftnext, poly rightnext) {
+	poly lnext = leftnext->next;
+	poly rnext = rightnext->next;
+	poly other = (struct Polynomial*)malloc(sizeof(struct Polynomial));
+	other->next = NULL;
+
+	while (lnext)
+	{
+		while (rnext)
+		{
+			other->power = lnext->power + rnext->power;
+			other->coefficient = lnext->coefficient * rnext->coefficient;
+			other = fill_leftnext(other, other->coefficient, other->power);
+			rnext = rnext->next;
+		}
+		lnext=lnext->next;
+		rnext = rightnext->next;
+	}
+
+	leftnext = free_next(leftnext, rightnext, other);
+
+	return leftnext;
+}
 void display(poly next) {
 	poly next_next = next->next;
+	int sign = 1;
 	while (next_next) {
+		if (sign == 1) {
+			if (next_next->coefficient == 1) {
+				printf("X^%d",next_next->power);
+			}
+			else{
+				printf("%dX^%d", next_next->coefficient, next_next->power);
+			}
+			sign = 2;
+			next_next = next_next->next;
+			continue;
+		}
 		if (next_next->coefficient > 0) {
 			printf("+");
 		}
-		else {
-			printf("-");
+
+		if (next_next->coefficient == 1) {
+			printf("X^%d", next_next->power);
 		}
-		printf("%dX^%d", next_next->coefficient, next_next->power);
+		else {
+			printf("%dX^%d", next_next->coefficient, next_next->power);
+		}
 		next_next = next_next->next;
 	}
 }
@@ -73,13 +129,21 @@ int main(void) {
 	printf("please enter the numbers of terms in the first polynomial\n");
 	scanf_s("%d", &times);
 	for (int i = 0; i < times; i++) {
-		head->leftnext = fill_leftnext(leftnext);
+		printf("enter the cofficients\n");
+		scanf_s("%d", &leftnext->coefficient);
+		printf("enter the power\n");
+		scanf_s("%d", &leftnext->power);
+		head->leftnext = fill_leftnext(leftnext, leftnext->coefficient,leftnext->power);
 	}
 	printf("left has been finished\n");
 	printf("please enter the numbers of terms in the second polynomial\n");
 	scanf_s("%d", &times);
 	for (int i = 0; i < times; i++) {
-		head->rightnext = fill_leftnext(rightnext);
+		printf("enter the cofficients\n");
+		scanf_s("%d", &rightnext->coefficient);
+		printf("enter the power\n");
+		scanf_s("%d", &rightnext->power);
+		head->rightnext = fill_leftnext(rightnext,rightnext->coefficient,rightnext->power);
 	}
 	printf("right has been finished\n");
 	
@@ -87,7 +151,9 @@ int main(void) {
 	display(leftnext);
 	printf("\n");
 	printf("another polynomial\n");
-
 	display(rightnext);
+	leftnext =  multipliacation(leftnext, rightnext);
+	printf("\n");
+	display(leftnext);
 	return 0;
 }
